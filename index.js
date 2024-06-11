ba5e2efda9ba4c6866cbe17761d3902cd2d7df01
officialryder1~ <kennethvic07@gmail.com>
2024-06-11 04:15:00 -0700

    initial push

---
+++ index.js
@@ -0,0 +1,28 @@
+const rock = document.getElementById('Rock').innerHTML;
+const paper = document.getElementById('Paper').innerHTML;
+const scissor = document.getElementById('Scissor').innerHTML;
+
+
+
+function computerMove(){
+    const moves = ['Rock', 'Paper', 'Scissor'];
+    const randomMove = Math.floor(Math.random() * moves.length);
+    return moves[randomMove];
+}
+
+function play(name){
+    let computer = computerMove()
+    if(name === computer){
+        document.getElementById('result').innerHTML = "Tie"
+    }else if(name === rock && computer === scissor){
+        document.getElementById('result').innerHTML = "You Won,  Computer pick: " + computer
+    }else if(name === paper && computer === rock){
+        document.getElementById('result').innerHTML = "You Won,  Computer pick: " + computer
+    }else if(name === scissor && computer === paper){
+        document.getElementById('result').innerHTML = "You Won,  Computer pick: " + computer
+    }else{
+        document.getElementById('result').innerHTML = "You Lose,  Computer pick: " + computer
+    }
+
+}
+
